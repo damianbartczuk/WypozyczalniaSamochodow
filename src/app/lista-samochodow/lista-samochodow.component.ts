ac4e6a3c3601aa5281a92b47d041d9f1cafa2a00
Damian <damian.bartczuk111@gmail.com>
2019-12-27 14:52:10 +0100

    poprawka widoku pojedyńczego samochodu

--- src/app/lista-samochodow/lista-samochodow.component.ts
+++ src/app/lista-samochodow/lista-samochodow.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Samochod} from '../Samochod';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-samochodow',
@@ -7,9 +9,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ListaSamochodowComponent implements OnInit {
 
-  constructor() { }
+  public samochody: Samochod[];
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
+
+    const headerDict = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Headers': '"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYmFydGN6dWsiLCJleHAiOjE1Nzc0MTU0OTgsImlhdCI6MTU3NzM5NzQ5OH0.EE2rdPr-ksIesw2pfJYmbeU_CWtXKFFn3qo1TukqwdFlafLzq-m36jAuK6y2PrT9wDC9G-zFDz3C8uPTSMY2vg'
+    };
+    let options = ({headers:headerDict});
+
+    this.http.get<Samochod[]>('http://localhost:9090/pobierz_samochody')//options
+      .subscribe(x => this.samochody = x);
   }
+
+
+
 
 }
--- src/app/pojedynczy-samochod/pojedynczy-samochod.component.ts
+++ src/app/pojedynczy-samochod/pojedynczy-samochod.component.ts
@@ -1,7 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Samochod} from '../Samochod';
-import {Observable} from 'rxjs';
-import {CarDataService} from '../serwisy/car-data.service';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pojedynczy-samochod',
@@ -10,17 +7,36 @@ import {CarDataService} from '../serwisy/car-data.service';
 })
 export class PojedynczySamochodComponent implements OnInit {
 
-  public samochody: Observable<Samochod[]>;
+  // public samochody: Samochod[];
 
-  constructor(private cds: CarDataService) {
+  @Input() logo: string;
+  @Input() marka: string;
+  @Input() model: string;
+  @Input() cenaZaDobe: number;
+  @Input() czyWypozyczony: boolean;
+  @Input() opis: number;
+  readonly ALERT_NIEDOSTEPNY_SAMOCHOD = 'Ten egzemplarz jest wypożyczony';
+
+  constructor() {
   }
 
   ngOnInit() {
-    this.samochody = this.cds.getAll();
+
+    // const headerDict = {
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+    //   'Access-Control-Allow-Headers': '"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    //   // 'Access-Control-Allow-Credentials' : 'true',
+    //   // "Accept": 'application/json',
+    //   'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYmFydGN6dWsiLCJleHAiOjE1Nzc0MTU0OTgsImlhdCI6MTU3NzM5NzQ5OH0.EE2rdPr-ksIesw2pfJYmbeU_CWtXKFFn3qo1TukqwdFlafLzq-m36jAuK6y2PrT9wDC9G-zFDz3C8uPTSMY2vg'
+    // };
+    // let options = ({headers:headerDict});
+    //
+    // this.http.get<Samochod[]>('http://localhost:9090/pobierz_samochody')//options
+    //   .subscribe(x => this.samochody = x);
   }
 
-  wypozyczSamochod(samochod: Samochod) {
-      samochod.czyWypozyczony = true;
+  wypozyczSamochod() {
+      this.czyWypozyczony = true;
   }
-
 }
