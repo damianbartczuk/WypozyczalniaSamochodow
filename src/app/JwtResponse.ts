64edf75a1b03add561ee813bd2f66d8a3bdd3247
Damian <damian.bartczuk111@gmail.com>
2020-01-08 22:40:45 +0100

    Implementacja metody wydzialająca token

--- src/app/JwtResponse.ts
+++ src/app/JwtResponse.ts
@@ -4,4 +4,10 @@ export class JwtResponse{
   constructor(token: string) {
     this.token = token;
   }
+
+  get tokenWithoutPrefix(): string {
+    console.log("to chyba taki token = "+this.token.split(' ')[1]);
+    return this.token.split(' ')[1];
+  }
 }
+
